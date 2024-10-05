7164432474d604fdb0f9c663c0af800ae9c9cb50
Jsep7 <jaimesepulveda112@gmail.com>
2024-10-05 19:09:40 -0400

    added routes

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons'; // Import the icon library
+import { colors } from '../styles/GlobalStyles'; // Ensure to import colors
 
 const HomeScreen: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to the Home Page!</Text>
+      
+      <View style={styles.bottomBar}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('About')}>
+          <Icon name="information-circle" size={30} color={colors.text} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('')}>
+          <Icon name="camera" size={30} color={colors.text} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Gallery')}>
+          <Icon name="images" size={30} color={colors.text} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -19,6 +36,22 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  bottomBar: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 80, // Increased height for the bottom bar
+    backgroundColor: '#3C3C3C',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
   },
 });
 
