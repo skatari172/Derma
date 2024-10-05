21ecb3b9f49d0674ce9db0c983b241b53b9d21fa
skatari172 <skatari172@gmail.com>
2024-10-05 17:49:12 -0400

    Login & Register page

---
+++ app/styles/GlobalStyles.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  background: '#2C2C2C',
+  title: '#D8CBAF',
+  inputBackground: '#3C3C3C',
+  inputBorder: '#D8CBAF',
+  button: '#D8CBAF',
+  text: '#FFFFFF',
+  placeholder: '#B0B0B0',
+};
